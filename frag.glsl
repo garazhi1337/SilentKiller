e71c36944df1da458ed74b21dcbd147b9e2f4a2c
Гаражи Админ <95549326+garazhi1337@users.noreply.github.com>
2024-12-12 20:56:22 +0300

    mesh lighning

--- frag.glsl
+++ frag.glsl
@@ -2,8 +2,11 @@
 
 out vec4 outColor;
 in vec2 textureCoords;
+in vec3 vertPosition;
+in vec3 vertNormal;
 
 uniform vec3 color;
+uniform vec3 eyePos;
 
 uniform sampler2D texture_diffuse1;
 uniform sampler2D texture_diffuse2;
@@ -18,5 +21,8 @@ uniform sampler2D texture_specular3;
 void main()
 {
 	//outColor = vec4(0.1, 0.2, 0.3, 1.f);
-	outColor = texture(texture_diffuse1, textureCoords);
+	vec4 ambient = texture(texture_diffuse1, textureCoords) * 0.15f;
+	float diffuseCoef = max(dot(-normalize(vertPosition - eyePos), vertNormal), 0.0f);
+	vec4 diffuse = texture(texture_diffuse1, textureCoords) * diffuseCoef;
+	outColor = ambient + diffuse;
 }
--- vert.glsl
+++ vert.glsl
@@ -1,17 +1,21 @@
 #version 330 core
 
-layout (location = 0) in vec3 aPos1;
-layout (location = 1) in vec3 normalCoords1;
-layout (location = 2) in vec2 textCoords1;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 normalCoords;
+layout (location = 2) in vec2 textCoords;
 
 uniform mat4 m;
 uniform mat4 v;
 uniform mat4 p;
 
 out vec2 textureCoords;
+out vec3 vertPosition;
+out vec3 vertNormal;
 
 void main()
 {
-	gl_Position = p * v * m * vec4(aPos1, 1.0f);
-	textureCoords = textCoords1;
+	gl_Position = p * v * m * vec4(aPos, 1.0f);
+	textureCoords = textCoords;
+	vertPosition = vec3(m * vec4(aPos, 1.0f));
+	vertNormal = mat3(m) * normalCoords;
 }
